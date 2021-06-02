c8ee057009c86825b03777f343777c623e97d991
Jeff <jf.gasp@gmail.com>
2021-06-02 23:36:27 +1100

    add anim translate

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var game = document.querySelector('.game__content');
 var gameFight = document.querySelector('.game__fight');
 var btnGame = document.querySelectorAll('.btn');
+var scorePlayer = 0;
+var scoreBot = 0;
 var hands;
 (function (hands) {
     hands["paper"] = "./images/icon-paper.svg";
     hands["scissors"] = "./images/icon-scissors.svg";
     hands["rock"] = "./images/icon-rock.svg";
 })(hands || (hands = {}));
-// == Score
-var scorePlayer = 0;
-var scoreBot = 0;
+var handClassSelectedPlayer;
+(function (handClassSelectedPlayer) {
+    handClassSelectedPlayer["paper"] = "game__fight__player--paper";
+    handClassSelectedPlayer["scissors"] = "game__fight__player--scissors";
+    handClassSelectedPlayer["rock"] = "game__fight__player--rock";
+})(handClassSelectedPlayer || (handClassSelectedPlayer = {}));
+var handClassSelectedBot;
+(function (handClassSelectedBot) {
+    handClassSelectedBot["paper"] = "game__fight__bot--paper";
+    handClassSelectedBot["scissors"] = "game__fight__bot--scissors";
+    handClassSelectedBot["rock"] = "game__fight__bot--rock";
+})(handClassSelectedBot || (handClassSelectedBot = {}));
 // Current main
 var handPlayer = 0;
 var handBot = 0;
@@ -18,51 +29,66 @@ var random = function () {
 };
 btnGame.forEach(function (hand) {
     hand.addEventListener('click', function (e) {
+        // == Attribute value : rock, paper or scissors
         handPlayer = random();
         handBot = random();
-        // == Récupère le bouton cliqué
+        // == Pick e.target button click
         var element = e.target;
-        // == Récupère le data-hand du bouton
+        // == Pick data-hand button
         var dataHand = element.dataset.hand;
-        var imgPlayer = document.createElement('div');
-        imgPlayer.classList.add("game__fight__player");
-        // result player
+        // == Create fight content player
+        var resultFightPlayer = document.createElement('div');
+        resultFightPlayer.classList.add('game__fight__player');
+        // == Create popup win or loose
+        var handPlayerSelected;
+        var handPlayerClassSelected = '';
+        // == Choice player (style)
         switch (dataHand) {
             case 'paper':
-                imgPlayer.innerHTML = "<img src='" + hands.paper + "' alt=''>";
-                imgPlayer.classList.add("game__fight__player--paper");
+                handPlayerSelected = hands.paper;
+                handPlayerClassSelected = handClassSelectedPlayer.paper;
                 break;
             case 'scissors':
-                imgPlayer.innerHTML = "<img src='" + hands.scissors + "' alt=''>";
-                imgPlayer.classList.add("game__fight__player--scissors");
+                handPlayerSelected = hands.scissors;
+                handPlayerClassSelected = handClassSelectedPlayer.rock;
                 break;
             case 'rock':
-                imgPlayer.innerHTML = "<img src='" + hands.rock + "' alt=''>";
-                imgPlayer.classList.add("game__fight__player--rock");
-                break;
-            default:
-                alert('Error');
+                handPlayerSelected = hands.rock;
+                handPlayerClassSelected = handClassSelectedPlayer.scissors;
                 break;
         }
-        var imgBot = document.createElement('div');
-        imgBot.classList.add("game__fight__bot");
-        // == result bot
+        // == Create fight content bot
+        var resultFightBot = document.createElement('div');
+        resultFightBot.classList.add('game__fight__bot');
+        var handBotSelected;
+        var handBotClassSelected = '';
+        // == Choice bot (style)
         switch (handBot) {
             case 0:
-                imgBot.innerHTML = "<img src='" + hands.paper + "' alt=''>";
-                imgBot.classList.add("game__fight__bot--paper");
+                handBotSelected = hands.paper;
+                handBotClassSelected = handClassSelectedBot.paper;
                 break;
             case 1:
-                imgBot.innerHTML = "<img src='" + hands.scissors + "' alt=''>";
-                imgBot.classList.add("game__fight__bot--scissors");
+                handBotSelected = hands.scissors;
+                handBotClassSelected = handClassSelectedBot.scissors;
                 break;
             case 2:
-                imgBot.innerHTML = "<img src='" + hands.rock + "' alt=''>";
-                imgBot.classList.add("game__fight__bot--rock");
+                handBotSelected = hands.rock;
+                handBotClassSelected = handClassSelectedBot.rock;
                 break;
         }
+        // == Insert img and style into bot div
+        resultFightPlayer.innerHTML = "<img src='" + handPlayerSelected + "' alt=''>";
+        resultFightPlayer.classList.add(handPlayerClassSelected);
+        // == Insert img and style into bot div
+        resultFightBot.innerHTML = "<img src='" + handBotSelected + "' alt=''>";
+        resultFightBot.classList.add(handBotClassSelected);
+        // == Insert div into game
         game.style.display = 'none';
-        gameFight.appendChild(imgPlayer);
-        gameFight.appendChild(imgBot);
+        gameFight.appendChild(resultFightPlayer);
+        gameFight.appendChild(resultFightBot);
+        // == Insert div isWin
+        var isWin = document.createElement('div');
+        gameFight.insertBefore(isWin, resultFightBot);
     });
 });
--- app.ts
+++ app.ts
@@ -1,81 +1,107 @@
-const game = document.querySelector<HTMLElement>('.game__content')!; 
+const game = document.querySelector<HTMLElement>('.game__content')!;
 const gameFight = document.querySelector('.game__fight')!;
 const btnGame = document.querySelectorAll('.btn');
-
-enum hands {
-    paper = './images/icon-paper.svg',
-    scissors = './images/icon-scissors.svg',
-    rock = './images/icon-rock.svg',
-}
-
-// == Score
 const scorePlayer: number = 0;
 const scoreBot: number = 0;
+
+enum hands {
+	paper = './images/icon-paper.svg',
+	scissors = './images/icon-scissors.svg',
+	rock = './images/icon-rock.svg',
+}
+enum handClassSelectedPlayer {
+	paper = 'game__fight__player--paper',
+	scissors = 'game__fight__player--scissors',
+	rock = 'game__fight__player--rock',
+}
+
+enum handClassSelectedBot {
+	paper = 'game__fight__bot--paper',
+	scissors = 'game__fight__bot--scissors',
+	rock = 'game__fight__bot--rock',
+}
 
 // Current main
 let handPlayer: number = 0;
 let handBot: number = 0;
 
-let random = ():number => {
+let random = (): number => {
 	return Math.floor(Math.random() * 3);
 };
 
 btnGame.forEach(hand => {
-	hand.addEventListener('click', (e) => {
+	hand.addEventListener('click', e => {
+		// == Attribute value : rock, paper or scissors
 		handPlayer = random();
 		handBot = random();
 
-        // == Récupère le bouton cliqué
-        const element = e.target as HTMLElement;
-        // == Récupère le data-hand du bouton
-        const dataHand = element.dataset.hand;
+		// == Pick e.target button click
+		const element = e.target as HTMLElement;
+		// == Pick data-hand button
+		const dataHand = element.dataset.hand;
 
-        let imgPlayer = document.createElement('div');
-        imgPlayer.classList.add("game__fight__player");
+		// == Create fight content player
+		let resultFightPlayer = document.createElement('div');
+		resultFightPlayer.classList.add('game__fight__player');
+		// == Create popup win or loose
 
-        // result player
-        switch (dataHand){
-            case 'paper':
-                imgPlayer.innerHTML = `<img src='${hands.paper}' alt=''>`;
-                imgPlayer.classList.add("game__fight__player--paper");
-                break;
-            case 'scissors' :
-                imgPlayer.innerHTML = `<img src='${hands.scissors}' alt=''>`;
-                imgPlayer.classList.add("game__fight__player--scissors");
-                break;
-            case 'rock' : 
-                imgPlayer.innerHTML = `<img src='${hands.rock}' alt=''>`;
-                imgPlayer.classList.add("game__fight__player--rock");
-                break;
-            default : alert('Error');
-                break;
-        }
-      
+		let handPlayerSelected;
+		let handPlayerClassSelected: string = '';
 
-		let imgBot = document.createElement('div');
-        imgBot.classList.add("game__fight__bot")
-
-        // == result bot
-        switch (handBot) {
-			case 0:
-                imgBot.innerHTML = `<img src='${hands.paper}' alt=''>`;
-                imgBot.classList.add("game__fight__bot--paper");
-
+		// == Choice player (style)
+		switch (dataHand) {
+			case 'paper':
+				handPlayerSelected = hands.paper;
+				handPlayerClassSelected = handClassSelectedPlayer.paper;
 				break;
-			case 1:
-                imgBot.innerHTML = `<img src='${hands.scissors}' alt=''>`;
-                imgBot.classList.add("game__fight__bot--scissors");
-                break;
-			case 2:
-                imgBot.innerHTML = `<img src='${hands.rock}' alt=''>`;
-                imgBot.classList.add("game__fight__bot--rock");
+			case 'scissors':
+				handPlayerSelected = hands.scissors;
+				handPlayerClassSelected = handClassSelectedPlayer.rock;
+				break;
+			case 'rock':
+				handPlayerSelected = hands.rock;
+				handPlayerClassSelected = handClassSelectedPlayer.scissors;
 				break;
 		}
 
-        
-		game.style.display = 'none';
-		gameFight.appendChild(imgPlayer);
-		gameFight.appendChild(imgBot);
-	});
-})
+		// == Create fight content bot
+		let resultFightBot = document.createElement('div');
+		resultFightBot.classList.add('game__fight__bot');
 
+		let handBotSelected;
+		let handBotClassSelected: string = '';
+
+		// == Choice bot (style)
+		switch (handBot) {
+			case 0:
+				handBotSelected = hands.paper;
+				handBotClassSelected = handClassSelectedBot.paper;
+				break;
+			case 1:
+				handBotSelected = hands.scissors;
+				handBotClassSelected = handClassSelectedBot.scissors;
+				break;
+			case 2:
+				handBotSelected = hands.rock;
+				handBotClassSelected = handClassSelectedBot.rock;
+				break;
+		}
+
+		// == Insert img and style into bot div
+		resultFightPlayer.innerHTML = `<img src='${handPlayerSelected}' alt=''>`;
+		resultFightPlayer.classList.add(handPlayerClassSelected);
+
+		// == Insert img and style into bot div
+		resultFightBot.innerHTML = `<img src='${handBotSelected}' alt=''>`;
+		resultFightBot.classList.add(handBotClassSelected);
+
+		// == Insert div into game
+		game.style.display = 'none';
+		gameFight.appendChild(resultFightPlayer);
+		gameFight.appendChild(resultFightBot);
+
+		// == Insert div isWin
+		let isWin = document.createElement('div');
+		gameFight.insertBefore(isWin, resultFightBot);
+	});
+});
